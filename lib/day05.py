32a94112713095d458c1cdf659d4aca635019f78
Benjamin Singleton <bsingleton@tenable.com>
2023-12-06 17:53:41 -0500

    Completed Day 5, part 1!

--- lib/day05.py
+++ lib/day05.py
@@ -11,8 +11,43 @@ def get_input_data(filename: str) -> list:
     return puzzle_input
 
 
+def parse_maps(puzzle_input: list) -> list:
+    my_maps = list()
+    my_maps.append([int(x) for x in puzzle_input[0].split(':')[1].strip().split(' ')])
+    line_number = 2
+    while line_number < len(puzzle_input):
+        if puzzle_input[line_number].strip() == '':
+            line_number += 1
+            continue
+        map_name = puzzle_input[line_number].strip(':')[0].strip()
+        map_lines = list()
+        line_number += 1
+        while line_number < len(puzzle_input) and puzzle_input[line_number].strip() != '':
+            map_lines.append([int(x) for x in puzzle_input[line_number].strip().split(' ')])
+            line_number += 1
+        my_maps.append(map_lines)
+    return my_maps
+
+
+def find_in_map(current_id: int, each_map: list) -> int:
+    # if this isn't in the map, then it stays the same
+    matching_id = current_id
+    for each_line in each_map:
+        if current_id >= each_line[1] and current_id <= each_line[1] + each_line[2]:
+            matching_id += each_line[0] - each_line[1]
+            break
+    return matching_id
+
+
 def day_05_part_1(puzzle_input: list) -> int:
-    pass
+    parsed_maps = parse_maps(puzzle_input=puzzle_input)
+    seed_to_location = dict()
+    for each_seed in parsed_maps[0]:
+        current_id = each_seed
+        for map_id in range(1, len(parsed_maps)):
+            current_id = find_in_map(current_id, parsed_maps[map_id])
+        seed_to_location[each_seed] = current_id
+    return min(seed_to_location.values())
 
 
 def main() -> None:
--- tests/test05.py
+++ tests/test05.py
@@ -8,11 +8,14 @@ from lib import day05
 
 
 class TestDay05(unittest.TestCase):
+    def test_parse_maps_01(self):
+        self.assertEqual(True, True)
+
     def test_day_05_part_1_short(self):
         self.assertEqual(day05.day_05_part_1(day05.get_input_data('inputs/input_05_short.txt')), 35)
 
     def test_day_05_part_1_full(self):
-        self.assertEqual(day05.day_05_part_1(day05.get_input_data('inputs/input_05.txt')), 0)
+        self.assertEqual(day05.day_05_part_1(day05.get_input_data('inputs/input_05.txt')), 261668924)
     
     def test_day_05_part_2_short(self):
         self.assertEqual(day05.day_05_part_2(day05.get_input_data('inputs/input_05_short.txt')), 0)
